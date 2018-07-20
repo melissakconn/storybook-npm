061aead1f194bdb1f65b1211d0691ae0ca7e9229
Melissa Conn <mkconn89@gmail.com>
2018-07-19 20:45:03 -0600

    storybook devDependencies

---
+++ packages/modal/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import { Container } from './styles'
+
+class modalEntry extends Component {
+  render(){
+    return (
+      <Container>
+        Hello this is my modal
+      </Container>
+    )
+  }
+}
+
+export default modalEntry
